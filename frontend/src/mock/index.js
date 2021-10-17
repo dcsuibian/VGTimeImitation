93433c24e8bec78bc27d66ae1d6b7c52e222355d
dcsuibian <dcsuibian@163.com>
2021-10-17 23:16:48 +0800

    make vue use proxy

--- frontend/src/mock/index.js
+++ frontend/src/mock/index.js
@@ -1,7 +1,8 @@
 // 如果是生产模式，不开启Mockjs
 let enableMock=process.env.NODE_ENV !== 'production'
 // 只在直接访问webpack dev server时才启用（也就是说nginx环境下不会启用）
-enableMock=enableMock?/https?:\/\/localhost:20119/.test(document.URL):false
+// enableMock=enableMock?/https?:\/\/localhost:20119/.test(document.URL):false
+enableMock=false
 
 if(enableMock){
     console.log('Mock.js 挂载中')
